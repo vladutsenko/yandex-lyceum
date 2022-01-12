1f99b67b5dce9da0350fd538b0b11c743d8faebe
vladutsenko <vladislav.lutsenko.23@gmail.com>
2022-01-12 21:19:46 +0300

    first commit

--- solution.py
+++ solution.py
@@ -1,23 +1,26 @@
-import sys
-
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from calc import Ui_MainWindow
-
-
-class MyWidget(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-        a = ""
-        self.pushButton.clicked.connect(self.run)
-
-    def run(self):
-        self.label.setText("OK")
-
+import pygame
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+    pygame.init()
+    pygame.display.set_caption('Жёлтый круг')
+    size = width, height = 300, 300
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    fps = 100
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                d = True
+                pos = event.pos
+                r = 0
+        screen.fill((0, 0, 255))
+        if d:
+            r = r + 1
+            pygame.draw.circle(screen, (255, 255, 0), (pos), r)
+        pygame.display.flip()
+        clock.tick(fps)
+    pygame.quit()
